d5d949bcbcf49d84b320f3827f0c3487dbeabd01
davirodrialencar-web <davirodrialencar@gmail.com>
2026-06-17 15:11:18 -0300

    17/06
    17/06

--- teste19.js
+++ teste19.js
@@ -1,5 +1,5 @@
 function calcularMedia (a,b,c) {
     return (a+b+c)/3
 }
-let resultado = calcularMedia(8,9,10)
+let resultado = calcularMedia(10,5,7)
 console.log(" A media do aluno é ",resultado)
